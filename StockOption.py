5a8bf7d69b3d28c7054fd70767684c61b781707e
s2434660 <s2434660@sms.ed.ac.uk>
2023-06-21 17:36:13 +0100

    change

--- StockOption.py
+++ StockOption.py
@@ -2,6 +2,23 @@ import math
 
 class StockOption(object):
     def __init__(self, S0, K, r, T, N, params):
+        """
+        Initialize the stock option base class.
+        Defaults to European call unless specified.
+
+        :param S0: initial stock price
+        :param K: strike price
+        :param r: risk-free interest rate
+        :param T: time to maturity
+        :param N: number of time steps
+        :param pu: probability at up state
+        :param pd: probability at down state
+        :param div: Dividend yield
+        :param is_put: True for a put option,
+                False for a call option
+        :param is_am: True for an American option,
+                False for a European option
+        """
         self.S0 = S0
         self.K = K
         self.r = r
@@ -9,14 +26,13 @@ class StockOption(object):
         self.N = max(1, N) # Ensure N have at least 1 time step
         self.STs = None  # Declare the stock prices tree
 
-        """ Optional parameters used by derived classes """
         self.pu = params.get("pu", 0)  # Probability of up state
         self.pd = params.get("pd", 0)  # Probability of down state
+
+
         self.div = params.get("div", 0)  # Dividend yield
         self.sigma = params.get("sigma", 0)  # Volatility
         self.is_call = params.get("is_call", True)  # Call or put
         self.is_european = params.get("is_eu", True)  # Eu or Am
-        
-        """ Computed values """
         self.dt = T/float(N)  # Single time step, in years
         self.df = math.exp(-(r-self.div) * self.dt)  # Discount factor
